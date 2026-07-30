508eccd02983b78e03ab7ca98809dcc9c1c91401
mywork03 <shubhamkhadekar11@gmail.com>
2026-07-30 21:57:34 +0530

    feat(utils): define application design tokens and grid default constants

---
+++ client/src/utils/constants.js
@@ -0,0 +1,21 @@
+/**
+ * Application Constant Definitions & Design System Tokens
+ */
+
+export const API_BASE_URL = '/api';
+
+export const GRID_DEFAULTS = {
+  MIN_GRID_SIZE: 10,
+  MAX_GRID_SIZE: 500,
+  DEFAULT_GRID_LENGTH: 50,
+  DEFAULT_GRID_WIDTH: 30,
+  NODE_VISUAL_WIDTH: 2.4,
+  NODE_VISUAL_HEIGHT: 2.0,
+  SNAP_STEP_METERS: 0.5
+};
+
+export const DISTANCE_THRESHOLDS = {
+  LOW_TRAVEL: 10,
+  MODERATE_TRAVEL: 15,
+  HIGH_FRICTION: 20
+};
